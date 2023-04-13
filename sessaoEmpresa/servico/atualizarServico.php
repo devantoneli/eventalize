d1793fc1e1553f0861098e0b7a642cf017cb249f
Antoneli <105890114+devantoneli@users.noreply.github.com>
2023-04-13 09:23:46 -0300

    Update and rename atualizarServico to atualizarServico.php

--- sessaoEmpresa/servico/atualizarServico.php
+++ sessaoEmpresa/servico/atualizarServico.php
@@ -8,7 +8,7 @@ $cd_local = $_POST['cd_local'];
 $cd_servico = $_POST['cd_servico'];
 $cd_tiposervico = $_POST['cd_tiposervico'];
 
-// BACK-END para salvar alterações feitas no arquivo edicaoServico.php
+// BACK-END para salvar alterações enviadas pelo arquivo edicaoServico.php
 
 var_dump($_POST);
 
