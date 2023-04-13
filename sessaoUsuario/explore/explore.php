10860f4ea5e24245061c01e18049718efc21bf6f
Antoneli <105890114+devantoneli@users.noreply.github.com>
2023-04-13 19:36:04 -0300

    Iniciando novo grupo Explore

---
+++ sessaoUsuario/explore/explore.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Explore</title>
+</head>
+<body>
+    <!-- MENU -->
+    <header>
+        <div class="logoHome">
+            <img src="img/index/logo.png" alt="logo">
+        </div>
+            <div class="headerHome">
+                <a href="">Início</a>
+                <a href="">Explore</a>
+                <a href="">Sobre Nós</a>
+            </div>
+            <div class="headerHomePerfil">
+                <button class="estiloVazio" id="entrarHome" onclick="novaSection(1)"><a>Entrar</a></button>
+                <button class="estiloVazio" id="entrarHome" onclick="novaSection(0)"><a>Cadastrar</a></button>
+            </div>
+            <button class="menuIcon" onclick="menuOpen()"><img  src="img/index/menuicon.svg" width="30px"></button>
+    </header>
+    <!-- FIM MENU -->
+
+    <!-- MENU -->
+
+</body>
+</html>
