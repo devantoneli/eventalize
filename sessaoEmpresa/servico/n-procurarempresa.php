6b404c76fa960dab94f2c4c06f1e670ace3d3f65
Antoneli <105890114+devantoneli@users.noreply.github.com>
2023-04-13 08:57:09 -0300

    Create n-procurarempresa.php

---
+++ sessaoEmpresa/servico/n-procurarempresa.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Procurar Empresa</title>
+</head>
+<body>
+    <form action="detalhesempresa.php">
+        <input type="text" placeholder="Código da empresa" name="cd_empresa">
+        <button type="submit">Pesquisar</button>
+    </form>
+</body>
+</html>
+
