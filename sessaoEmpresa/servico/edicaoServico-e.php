4f3e8341b6918197af3634201db01113be9bc094
Antoneli <105890114+devantoneli@users.noreply.github.com>
2023-04-14 10:01:36 -0300

    Programando back e front ao mesmo tempo
    Foi criada uma tabela no banco de dados pra tb_postagens e as relações muitos pra muitos

--- sessaoEmpresa/servico/edicaoServico-e.php
+++ sessaoEmpresa/servico/edicaoServico-e.php
@@ -2,8 +2,8 @@
 
 // AQUI ESTAREMOS SELCIONANDO AS INFORMAÇÕES DO SERVIÇO SELECIONADO, PARA QUE A EMPRESA POSSA EDITÁ-LOS
 
-if (isset($_GET['cd_servico'])) {
-    $cd_servico = $_GET['cd_servico'];
+// if (isset($_GET['cd_servico'])) {
+    // $cd_servico = $_GET['cd_servico'];
 
     $servername = "localhost";
     $username = "root";
@@ -12,13 +12,13 @@ if (isset($_GET['cd_servico'])) {
 
     $conn = new mysqli($servername, $username, $password, $db_name);
 
-    $sql = "SELECT * FROM tb_servico WHERE cd_servico = $cd_servico";
+    $sql = "SELECT * FROM tb_servico WHERE cd_servico = 800";
     $result = $conn->query($sql);
 
     if ($result->num_rows == 1) {
         $row = $result->fetch_assoc();
     }
-}
+// }
 
 ?>
 
@@ -28,7 +28,7 @@ if (isset($_GET['cd_servico'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/adicionarServico.css">
+    <link rel="stylesheet" href="estiloServico-e.css">
     <link rel="icon" href="imgCriandoServico/logo.png">
     <link href="https://fonts.googleapis.com/css2?family=League+Spartan&display=swap" rel="stylesheet">
     <title>Criar Serviço/Pacote</title>
@@ -57,7 +57,7 @@ if (isset($_GET['cd_servico'])) {
 
 <!-- SESSAO SERVICO -->  
     <section class="servico">
-        <form class="grid-Principal" action="editarservico.php" method="POST">
+        <form class="grid-Principal" action="atualizarServico-e.php" method="POST">
             <div class="digitacao">
                 <input type="hidden" name="cd_servico" value="<?php echo($row["cd_servico"]);?>">
                 <input class="input-Digita" type="text" placeholder="Título do serviço" name="nm_servico" value="<?php if (isset($row)) { echo $row['nm_servico']; } ?>">
--- sessaoUsuario/explore/explore.php
+++ sessaoUsuario/explore/explore.php
@@ -1,9 +1,33 @@
+<?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "db_eventalize";
+
+$conn = new mysqli($servername, $username, $password, $db_name);
+
+$sql = "SELECT * FROM tb_servico";
+$result = $conn->query($sql);
+
+$result_query = mysqli_query($conn, $sql) or die(' Erro na query:' . $sql . ' ' . mysqli_error($conn));
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+}else{
+    echo ('rolou algum erro');
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/sistema/eventalize/css/estilo.css">
+    <link rel="stylesheet" href="estiloExplore.css">
     <title>Explore</title>
 </head>
 <body>
@@ -13,7 +37,7 @@
             <img src="img/index/logo.png" alt="logo">
         </div>
             <div class="headerHome">
-                <a href="">Início</a>
+                <a href="../../index.html">Início</a>
                 <a href="">Explore</a>
                 <a href="">Sobre Nós</a>
             </div>
@@ -25,7 +49,71 @@
     </header>
     <!-- FIM MENU -->
 
-    <!-- MENU -->
+    <!-- CORPO - GRID DE POSTAGENS -->
 
+<?php
+if ($result->num_rows > 0) {
+while($row = $result->fetch_assoc()) {
+    echo '<div class="col-4"><div class="card" style="width: 18rem;">
+    <img src="'.$row["url_img"].'" class="card-img-top borrado-amarelo" alt="'. $row["nm_pokemon"] .'">
+    <div class="card-body borrado-amarelo">
+      <h5 class="card-title">'. $row["nm_pokemon"] .'</h5>
+      <p class="card-text">'. $row["ds_tipo1"] . "/" .$row["ds_tipo2"] .'</p>
+      <form action="deletar.php">
+       <input type="hidden" name="cd_pokemon" value="'. $row["cd_pokemon"] .'">
+       <input type="submit" class="btn btn-warning" value="Deletar">
+      </form>
+      <form action="alterarsele.php">
+      <input type="hidden" name="cd_pokemon" value="'. $row["cd_pokemon"] .'">
+      <input type="submit" class="btn btn-primary" value="Alterar">
+     </form>
+    </div>
+  </div></div>';
+}
+}else {
+    echo "0 results";
+}
+$conn->close();
+?>
+    <div class="row">
+        <div class="column">
+            <img src="">
+            <img src="<?php echo $row['url_imgcapa'];?>">
+            <img src="<?php echo $row['url_img2'];?>">
+            <img src="<?php echo $row['url_img3'];?>">
+            <img src="nature.jpg">
+            <img src="mist.jpg">
+            <img src="paris.jpg">
+        </div>
+        <div class="column">
+            <img src="underwater.jpg">
+            <img src="ocean.jpg">
+            <img src="wedding.jpg">
+            <img src="mountainskies.jpg">
+            <img src="rocks.jpg">
+            <img src="underwater.jpg">
+        </div>
+        <div class="column">
+            <img src="wedding.jpg">
+            <img src="rocks.jpg">
+            <img src="falls2.jpg">
+            <img src="paris.jpg">
+            <img src="nature.jpg">
+            <img src="mist.jpg">
+            <img src="paris.jpg">
+        </div>
+        <div class="column">
+            <img src="underwater.jpg">
+            <img src="ocean.jpg">
+            <img src="wedding.jpg">
+            <img src="mountainskies.jpg">
+            <img src="rocks.jpg">
+            <img src="underwater.jpg">
+        </div>
+    </div>
+
+    <!-- FIM CORPO -->
+    
+    <script src="/sistema/eventalize/js/menu.js"></script>
 </body>
 </html>
