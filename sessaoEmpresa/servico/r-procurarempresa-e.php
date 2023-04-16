d5dfd88b6f7d58259ecf482997392ca59df7e5fe
Antoneli <105890114+devantoneli@users.noreply.github.com>
2023-04-15 22:37:12 -0300

    Outro teste

--- sessaoEmpresa/servico/r-procurarempresa-e.php
+++ sessaoEmpresa/servico/r-procurarempresa-e.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 <head>
     <!-- RASCUNHO FRONT-END formulario para procurar serviços baseados em uma empresa enviada para consultarServicos.php
-    ESTE ARQUIVO NÃO PODE EXISTIR, É ALGO QUE JÁ TEM QUE SER DETERMINADO NO LOGIN -->
+    ESTE ARQUIVo NÃO PODE EXISTIR, É ALGO QUE JÁ TEM QUE SER DETERMiNADO NO LOGIN -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
