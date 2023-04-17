0e29ee56d4dcdf51fb8d916414a8110233428587
milena <milenacnobre@hotmail.com>
2023-04-16 22:28:40 -0300

    backend pesquisar pedido para postagem empresa

--- sessaoEmpresa/postagem/selecionarPedido-e.php
+++ sessaoEmpresa/postagem/selecionarPedido-e.php
@@ -2,9 +2,7 @@
 
 //NESSE ARQUIVO, ESTAREMOS SELECIONANDO O PEDIDO QUE A EMPRESA DESEJA FAZER A POSTAGEM
 
-if (isset($_GET["nm_cliente"])) {
-    $nm_cliente = "%".trim($_POST["nm_cliente"])."%";
-
+$nm_cliente = $_POST["nm_cliente"];
 
 $servername = "localhost";
 $username = "root";
@@ -17,21 +15,25 @@ if($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
 
-
-$sql = "SELECT c.nm_cliente FROM tb_pedido AS p
-            JOIN tb_infopagamento AS ip ON
-                ip.cd_pagamento = p.cd_infopagamento
-                    JOIN tb_cliente AS c on c.cd_cliente = ip.cd_cliente
-                        WHERE c.cd_cliente = $nm_cliente";
+$sql = "SELECT * FROM vwpedidocliente WHERE  nm_cliente LIKE '%$nm_cliente%'";
+// $sql = "SELECT * FROM vwpedidocliente WHERE cd_empresa = $cd_empresa AND nm_cliente = %$nm_cliente%";
 
 $result_query = mysqli_query($conn, $sql) or die(' Erro na query:' . $sql . ' ' . mysqli_error($conn));
 $row = mysqli_fetch_assoc($result_query);
 
-
 if(mysqli_num_rows($result_query) > 0){
+    echo "<table>";
     while($row = mysqli_fetch_assoc($result_query)){
-        echo  $row["nm_cliente"];
-    }
+        echo
+        // '<td>'. $row["cd_pedido"]; '</td>
+        '<tr>
+        <td>' . $row["nm_cliente"] . '</td>
+        <td>' . $row["dt_pedido"] . '</td>
+        <td>R$' . $row["vl_pedido"] . '</td>
+        <tr>';
 }
+    }else{
+        echo "Nenhum registro encontrado";
 }
+
 ?>
