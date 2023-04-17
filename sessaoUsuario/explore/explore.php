ae7979621f29c250f5ebc15b0b1f7c27c885a09e
Antoneli <105890114+devantoneli@users.noreply.github.com>
2023-04-17 00:11:28 -0300

    Desenvolvendo Explore, back e front

--- sessaoUsuario/explore/explore.php
+++ sessaoUsuario/explore/explore.php
@@ -50,66 +50,25 @@ if ($result->num_rows > 0) {
     <!-- FIM MENU -->
 
     <!-- CORPO - GRID DE POSTAGENS -->
-
-<?php
-if ($result->num_rows > 0) {
-while($row = $result->fetch_assoc()) {
-    echo '<div class="col-4"><div class="card" style="width: 18rem;">
-    <img src="'.$row["url_img"].'" class="card-img-top borrado-amarelo" alt="'. $row["nm_pokemon"] .'">
-    <div class="card-body borrado-amarelo">
-      <h5 class="card-title">'. $row["nm_pokemon"] .'</h5>
-      <p class="card-text">'. $row["ds_tipo1"] . "/" .$row["ds_tipo2"] .'</p>
-      <form action="deletar.php">
-       <input type="hidden" name="cd_pokemon" value="'. $row["cd_pokemon"] .'">
-       <input type="submit" class="btn btn-warning" value="Deletar">
-      </form>
-      <form action="alterarsele.php">
-      <input type="hidden" name="cd_pokemon" value="'. $row["cd_pokemon"] .'">
-      <input type="submit" class="btn btn-primary" value="Alterar">
-     </form>
-    </div>
-  </div></div>';
-}
-}else {
-    echo "0 results";
-}
-$conn->close();
-?>
     <div class="row">
-        <div class="column">
+        <?php
+        if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo '<div class="column">
             <img src="">
-            <img src="<?php echo $row['url_imgcapa'];?>">
-            <img src="<?php echo $row['url_img2'];?>">
-            <img src="<?php echo $row['url_img3'];?>">
+            <img src="'. $row['url_imgcapa'].'">
+            <img src="'.$row['url_img2'].'">
+            <img src="'.$row['url_img3'].'">
             <img src="nature.jpg">
             <img src="mist.jpg">
             <img src="paris.jpg">
-        </div>
-        <div class="column">
-            <img src="underwater.jpg">
-            <img src="ocean.jpg">
-            <img src="wedding.jpg">
-            <img src="mountainskies.jpg">
-            <img src="rocks.jpg">
-            <img src="underwater.jpg">
-        </div>
-        <div class="column">
-            <img src="wedding.jpg">
-            <img src="rocks.jpg">
-            <img src="falls2.jpg">
-            <img src="paris.jpg">
-            <img src="nature.jpg">
-            <img src="mist.jpg">
-            <img src="paris.jpg">
-        </div>
-        <div class="column">
-            <img src="underwater.jpg">
-            <img src="ocean.jpg">
-            <img src="wedding.jpg">
-            <img src="mountainskies.jpg">
-            <img src="rocks.jpg">
-            <img src="underwater.jpg">
-        </div>
+                </div>';
+        }
+        }else {
+            echo "0 results";
+        }
+        $conn->close();
+        ?>
     </div>
 
     <!-- FIM CORPO -->
