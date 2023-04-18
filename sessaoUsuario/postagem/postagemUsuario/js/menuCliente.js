806ed3e175905da3b00db4848234c741e6b37ed2
Palloma Nobre <pallomaneves2011@gmail.com>
2023-04-17 21:13:34 -0300

    postagem

--- sessaoUsuario/postagem/postagemUsuario/js/menuCliente.js
+++
@@ -1,9 +0,0 @@
-function menuOpen(){
-    let menuMobile = document.querySelector('.menuPerfil');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-    }
-    else {
-        menuMobile.classList.add('open');
-    }
-}
