46a2fe8b4887779456f79e38e8efc0f78b1a9465
piettra <piettraneves@gmail.com>
2023-04-28 22:51:08 -0300

    tela postagem atualizada - Palloma
    menu do home adicionado + imagens do carrossel ajustadas + implementação de funcionalidade do carrossel (obs: quando adicionado 2 slides fixamente, o carrossel para de funcionar, e bagunça o layout. e em banco precisamos ter uma postagem com + de um pacote.)

--- sessaoUsuario/postagem.php
+++ sessaoUsuario/postagem.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/estilo.css">
     <link rel="stylesheet" href="css/postagem.css">
     <link rel="icon" href="../img/icones/logo.png">
     <title>Postagem Empresa</title>
@@ -15,24 +16,25 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "db_eventalize";
-
+// $cd_postagem = $_GET['cd_postagem'];
     $conn = new mysqli($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
     die("Não foi possível conectar ao banco de dados: " . $conn->connect_error);
     }
 
-    // $cd_postagem = $_GET ['cd_postagem'];
-    //SELECT nm_postagem, ds_postagem, url_imgcapa, url_img2, url_img3 FROM tb_postagem WHERE cd_postagem = $cd_postagem; (pra quando o da raiza estiver pronto)
-    $sql = 'SELECT nm_postagem, ds_postagem, url_imgcapa, url_img2, url_img3 FROM tb_postagem WHERE cd_postagem = 17';
+    $cd_postagem = $_GET ['cd_postagem'];
+    // $sql = 'SELECT nm_postagem, ds_postagem, url_imgcapa, url_img2, url_img3 FROM tb_postagem WHERE cd_postagem = '.$cd_postagem.''; //(pra quando o da raiza estiver pronto)
+    $sql = 'SELECT nm_postagem, ds_postagem, url_imgcapa, url_img2, url_img3 FROM tb_postagem WHERE cd_postagem = 19';
     $result = $conn->query($sql);
     //SELECT PARA PEGAR AS INFORMAÇÕES DE EMPRESA
-    $sql2 = 'SELECT e.nm_fantasia FROM tb_empresa as e JOIN tb_postagem as p ON e.cd_empresa = p.cd_empresa WHERE p.cd_postagem = 17';
+    // $sql2 = 'SELECT e.nm_fantasia FROM tb_empresa as e JOIN tb_postagem as p ON e.cd_empresa = p.cd_empresa WHERE p.cd_postagem = '.$cd_postagem.'';
+    $sql2 = 'SELECT e.nm_fantasia FROM tb_empresa as e JOIN tb_postagem as p ON e.cd_empresa = p.cd_empresa WHERE p.cd_postagem = 19';
     $result2 = $conn->query($sql2);
     $row2 = $result2 -> fetch_assoc();
     // $empresa = $result2->fetch(PDO::FETCH_ASSOC);
-    //SELECT vwcodigospostagem WHERE cd_postagem = $cd_postagem; (pra quando o da raiza estiver pronto)
-    $sql3 = 'SELECT * FROM vwcodigospostagem WHERE cd_postagem = 17';
+    // $sql3 = 'SELECT vwcodigospostagem WHERE cd_postagem = '.$cd_postagem.''; //(pra quando o da raiza estiver pronto)
+    $sql3 = 'SELECT * FROM vwcodigospostagem WHERE cd_postagem = 45';
     $result3 = $conn->query($sql3);
 
     // $sql4= "SELECT * FROM tb_pacote WHERE cd_pacote = ".$row3['cd_pacote'] ." ";
@@ -48,7 +50,10 @@ $dbname = "db_eventalize";
                     $largura = 100; 
                     $altura = 100;
                     $largura_capa = 100;
-                    $altura_capa = 100;  
+                    $altura_capa = 100;
+                    
+
+
             echo '
             <head>
     <meta charset="UTF-8">
@@ -59,38 +64,23 @@ $dbname = "db_eventalize";
     <title>Postagem Empresa</title>
     </head>
             <body>
+                <header>
+                    <div class="logoHome">
+                        <img src="../img/index/logo.png" alt="logo">
+                    </div>
+                        <div class="headerHome">
+                            <a href="">Início</a>
+                            <a href="../sessaoUsuario/explore.php">Explore</a>
+                            <a href="">Sobre Nós</a>
+                        </div>
+                        <div class="headerHomePerfil">
+                            <button class="estiloVazio" id="entrarHome" onclick="novaSection(1)"><a href="../login.php">Entrar</a></button>
+                            <button class="estiloVazio" id="entrarHome" onclick="novaSection(0)"><a href="../login.php">Cadastrar</a></button>
+                        </div>
+                        <button class="menuIcon" onclick="menuOpen()"><img  src="../img/index/menuicon.svg" width="30px"></button>
+                </header>
+
                 <div class="grid-container"> <!--DIV QUE CARREGA O LAYOUT GRID DA PÁGINA TODA-->
-                <!-- MENU -->
-                    <div class="header">
-                      <div class="logo">
-                          <img src="../img/icones/logoBranca.svg" alt="">
-                      </div>
-                      <div class="menu">
-                          <a href="">Início</a>
-                          <a href="">Inspira-se</a>
-                          <a href="">Sobre Nós</a>
-                          <a href="">Contato</a>
-                      </div>
-                  
-                      <div class="headerPesquisa">
-                            <input type="text" placeholder="Procure Serviços">
-                            <img src="../img/icones/icon-lupa.svg" alt="" width="30px">
-                          </div>
-                          <div class="headerClientePerfil" >
-                              <!-- <div class="iconCliente"> -->
-                                  <a href="#" class="carrinho"><img src="../img/icones/icon-carrinho.svg" alt="Carrinho"></a>
-                                  <a href="#" class ="notificacao"><img src="../img/icones/icon-notificacao.svg" alt="Notificações"></a>
-                                <!-- </div> -->
-                          <button class="menuIcon" onclick="menuOpen()"><img  src="../img/icones/vector.svg" style="height: 50px;" width="30px"></button>
-                          </div>
-                          <section class="menuPerfil">
-                            <a href="">Perfil</a>
-                            <a href="">Postagens</a>
-                            <a href="" style="margin-bottom: 20%">Histórico de Compras</a>
-                            <a href="">Configurações</a>
-                            <a href="">Sair</a>
-                        </section>
-                  </div>
                    <!-- INICIO POSTAGENS -->
                   <div class="inicioPostagens">
                     <div class="fotoDestaque">
@@ -142,6 +132,12 @@ $dbname = "db_eventalize";
                     }
                     if (mysqli_num_rows($result4) > 0){
                         while ($row4 = $result4 -> fetch_assoc()) {
+                            $capa_img_pacote = getimagesize($row4["url_imgcapa"]);
+                            $info_imagem_pacote = getimagesize($row4["url_img2"]. $row4["url_img3"] );
+                            $largura_capa_pacote = 100;
+                            $altura_capa_pacote = 100;
+                            $largura_pacote = 100; 
+                            $altura_pacote = 100;
                         // aqui vem o carrosel da pa 
                         echo' <!-- INICIO EXIBIÇÕES DE PACOTES -->
                 <div class="infoDescricao">
@@ -154,82 +150,45 @@ $dbname = "db_eventalize";
                 <div class="slides-container">
                     <!-- slides -->
                     <div class="slide" data-slide>
-                    <div class="tituloInfoPacote" '.$row4['nm_pacote'].'>
+                    <div class="tituloInfoPacote" >
                         <img src="../img/icones/icon-decoracao-detalhes-pacote.png" alt="">
-                        <h3>Decoração florida para casamentos elegantes</h3>
+                        <h3>'.$row4['nm_pacote'].'</h3>
                     </div>
                     <div class="fotoDestaquePacote">
-                        <img src="../bancoImagens/postagens/fotodestaque.jpg" class="active" alt="">
+                        <img src="'.$row4["url_imgcapa"].'" style="width:'.$largura_capa_pacote.'%;height:'.$altura_capa_pacote.'%;">
                     </div>
             
                     <div class="fotoLateralPacote">
-                        <img src="../bancoImagens/postagens/fotolateral.jpg" alt="">
-                        <img src="../bancoImagens/postagens/fotolateral2.jpg" alt="">
+                        <img src="'.$row4["url_img2"].'" style="width:'.$largura_pacote.'%;height:'.$altura_pacote.'%;">
+                        <img src="'.$row4["url_img3"].'" style="width:'.$largura_pacote.'%;height:'.$altura_pacote.'%;">
                     </div>
             
                     <div class="descricaoInfoPacote">
-                        <h3>Decoração repleta de flores, das mais delicadas às mais sofisticadas, com móveis e talheres em tons pastéis e funcionários dedicados...</h3>
+                        <h3>'.$row4['ds_pacote'].'</h3>
                     </div>
+                    
+                    <div class="slide" data-slide>
+                    <div class="tituloInfoPacote" >
+                        <img src="../img/icones/icon-decoracao-detalhes-pacote.png" alt="">
+                        <h3>'.$row4['nm_pacote'].'</h3>
+                    </div>
+                    <div class="fotoDestaquePacote">
+                        <img src="'.$row4["url_imgcapa"].'" style="width:'.$largura_capa_pacote.'%;height:'.$altura_capa_pacote.'%;">
+                    </div>
+            
+                    <div class="fotoLateralPacote">
+                        <img src="'.$row4["url_img2"].'" style="width:'.$largura_pacote.'%;height:'.$altura_pacote.'%;">
+                        <img src="'.$row4["url_img3"].'" style="width:'.$largura_pacote.'%;height:'.$altura_pacote.'%;">
+                    </div>
+            
+                    <div class="descricaoInfoPacote">
+                        <h3>'.$row4['ds_pacote'].'</h3>
+                    </div>
+                    
             
                     
                 </div>
-                    <div class="slide" data-slide>
-                    <div class="tituloInfoPacote">
-                        <img src="../img/icones/icon-decoracao-detalhes-pacote.png" alt="">
-                        <h3>Decoração florida para casamentos elegantes</h3>
-                    </div>
-                    <div class="fotoDestaquePacote">
-                        <img src="../bancoImagens/postagens/fotodestaque.jpg" class="active" alt="">
-                    </div>
-            
-                    <div class="fotoLateralPacote">
-                        <img src="../bancoImagens/postagens/fotolateral.jpg" alt="">
-                        <img src="../bancoImagens/postagens/fotolateral2.jpg" alt="">
-                    </div>
-            
-                    <div class="descricaoInfoPacote">
-                        <h3>Decoração repleta de flores, das mais delicadas às mais sofisticadas, com móveis e talheres em tons pastéis e funcionários dedicados...</h3>
-                    </div>
-            
-                    
-                </div>
-                    <div class="slide" data-slide>
-                    <div class="tituloInfoPacote">
-                        <img src="../img/icones/icon-decoracao-detalhes-pacote.png" alt="">
-                        <h3>Decoração florida para casamentos elegantes</h3>
-                    </div>
-                    <div class="fotoDestaquePacote">
-                        <img src="../bancoImagens/postagens/fotodestaque.jpg" class="active" alt="">
-                    </div>
-            
-                    <div class="fotoLateralPacote">
-                        <img src="../bancoImagens/postagens/fotolateral.jpg" alt="">
-                        <img src="../bancoImagens/postagens/fotolateral2.jpg" alt="">
-                    </div>
-            
-                    <div class="descricaoInfoPacote">
-                        <h3>Decoração repleta de flores, das mais delicadas às mais sofisticadas, com móveis e talheres em tons pastéis e funcionários dedicados...</h3>
-                    </div>
-            
                    
-                </div>
-                    <div class="slide" data-slide>
-                    <div class="tituloInfoPacote">
-                        <img src="../img/icones/icon-decoracao-detalhes-pacote.png" alt="">
-                        <h3>Decoração florida para casamentos elegantes</h3>
-                    </div>
-                    <div class="fotoDestaquePacote">
-                        <img src="../bancoImagens/postagens/fotodestaque.jpg" class="active" alt="">
-                    </div>
-            
-                    <div class="fotoLateralPacote">
-                        <img src="../bancoImagens/postagens/fotolateral.jpg" alt="">
-                        <img src="../bancoImagens/postagens/fotolateral2.jpg" alt="">
-                    </div>
-            
-                    <div class="descricaoInfoPacote">
-                        <h3>Decoração repleta de flores, das mais delicadas às mais sofisticadas, com móveis e talheres em tons pastéis e funcionários dedicados...</h3>
-                    </div>
             
                    
                 </div>
@@ -251,7 +210,7 @@ $dbname = "db_eventalize";
             
                
             
-                '<!-- MAIS SOBRE A EMPRESA -->
+                echo '<!-- MAIS SOBRE A EMPRESA -->
                 <div class="maisSobre">
                     <h3>Mais sobre a empresa</h3>
                     <div class="gridMaisSobre">
@@ -281,8 +240,8 @@ $dbname = "db_eventalize";
 
 ?>
 
-        <script src="../js/menu.js"></script>
-            <script src="../sessaoUsuario/js/carousel.js"></script>
+        <script src="/js/menu.js"></script>
+        <script src="../sessaoUsuario/js/carousel.js"></script>
 
 
 
