ac16abd728886d4bc1eff1ab420289f0914a384a
piettrann <piettraneves@gmail.com>
2023-05-01 19:21:03 -0300

    arrumando o menu de empresa
    adicionando foto de perfil

--- sessaoEmpresa/index-e.php
+++ sessaoEmpresa/index-e.php
@@ -9,13 +9,14 @@ include('../protect.php');
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <!-- FRONT-END tela incial da empresa, redirecionada após passar pela validação de cadastro ou login -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/estilo-e.css">
+    <!-- <link rel="stylesheet" href="css/menu-e.css"> -->
     <link href="https://fonts.googleapis.com/css2?family=League+Spartan&display=swap" rel="stylesheet">
     <link rel="icon" href="../img/index/logo.png">
     <title>Início Empresa - Eventalize</title>
@@ -53,7 +54,9 @@ include('../protect.php');
                     <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
                 </svg>
 
-                <div id="inserirPerfil"></div>
+                <div id="inserirPerfil">
+                    <img src="<?php echo $_SESSION['url_fotoperfil'];?>" alt="">
+                </div>
 
                 <section id="menuPerfil">
                     <a href="perfilEmpresa.php"><h4>Perfil</h4></a>
