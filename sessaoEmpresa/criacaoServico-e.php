fb0ffa698c1fba433cae026a9579423b76225aa4
milena <milenacnobre@hotmail.com>
2023-05-02 16:15:03 -0300

    arrumando menu criar serviço

--- sessaoEmpresa/criacaoServico-e.php
+++ sessaoEmpresa/criacaoServico-e.php
@@ -154,7 +154,7 @@ $conn->close();
     
                 <section id="menu">
                     <!-- <a href=""><h5>Pedidos</h5></a> -->
-                    <a href="criacaoServico-e.html"><h5>Cria Serviço ou Pacote</h5></a>
+                    <a href="criacaoServico-e.php"><h5>Criar Serviço ou Pacote</h5></a>
                     <a href="selecaoPedido-e.php"><h5>Criar Postagens</h5></a>
                 </section>
     
