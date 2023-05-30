fcd56bdb742a3224cd9b1473b35c16be174aebd9
palloma <pallomaneves2011@gmail.com>
2023-05-30 19:06:25 -0300

    arrumando detalhes das telas carrinho

--- sessaoCliente/carrinhoPagamento.php
+++ sessaoCliente/carrinhoPagamento.php
@@ -71,7 +71,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }elseif ($opcao == "1") {
       echo "<div class='formCredito'>
       <input type='text' placeholder='Nome do Titular'>
-      <div class='formdiv1'>
+      <div class='formdiv'>
           <input type='number' placeholder='Número do Cartão'>
           <input type='number' placeholder='CVV' style='padding-left: 8%;'>
       </div>
@@ -95,7 +95,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } elseif ($opcao == "2") {
       echo "<div class='formCredito'>
       <input type='text' placeholder='Nome do Titular'>
-      <div class='formdiv1'>
+      <div class='formdiv'>
           <input type='number' placeholder='Número do Cartão'>
           <input type='number' placeholder='CVV' style='padding-left: 8%;'>
       </div>
